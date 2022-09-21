1701c990cecca9c4bac10e086b3acc6fbf0c9612
Bastien Lutz <bastien.lutz@in2code.de>
2022-09-21 20:29:33 +0200

    [TASK] setup fe toolchain

---
+++ build/webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    mode: 'development',
+    context: path.resolve(__dirname, 'Assets'),
+    entry: {
+        default: ['./css/default.css'],
+        navigation: ['./javascript/navigation.js', './css/navigation.css'],
+        product: ['./css/product.css'],
+    },
+    output: {
+        path: path.resolve(__dirname, '../public/assets'),
+        filename: 'javascript/[name].js',
+        chunkFilename: 'javascript/[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules\/(?!(scroll-js|dom7|flatpickr|swiper)\/).*/,
+                use: ["babel-loader"],
+                parser: {
+                    amd: false
+                }
+            },
+            {
+                test: /\.css/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: "css-loader",
+                        options: {
+                            url: false
+                        }
+                    }
+                ]
+            },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].css',
+            chunkFilename: 'css/[name].css'
+        }),
+    ],
+};
